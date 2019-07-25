7f92e226af006fa5082c676f73a28ead7594c21e
Nicola Baccillieri <n.baccillieri@reply.it>
2019-07-25 14:55:17 +0200

    [ADD] better model with it's loss graph (5h of training)

--- ai/neuralNetwork/KerasModel.py
+++ ai/neuralNetwork/KerasModel.py
@@ -2,7 +2,6 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import keras
 from keras.models import Sequential
 from keras.optimizers import Adam
 from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
